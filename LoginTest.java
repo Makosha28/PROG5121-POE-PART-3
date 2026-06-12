87d50d5e38282fcbd7247e632234dad70f10d3e1
Makosha28 <st10534919@rcconnect.edu.za>
2026-06-12 09:08:31 +0200

    Remove commented license and template code from LoginTest

--- LoginTest.java
+++ LoginTest.java
@@ -1,7 +1,5 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
- */
 package com.mycompany.main;
 
 import java.util.Scanner;
